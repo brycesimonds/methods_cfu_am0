29810acb32270270b7fac6ea15138a62e5f63b2e
Bryce Simonds <bsimonds14@gmail.com>
2022-04-21 16:50:11 -0600

    Show all exercises in this file have been completed

--- methods_variables.rb
+++ methods_variables.rb
@@ -24,9 +24,41 @@ easy_enough.length
 user_name = "coco_11am"
 last_login = "12/09/2021"
 
-# 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do.
-  # Write the methods out on the lines below, with an explanation in your own words of how they work.
+# 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call
+# them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do.
+# Write the methods out on the lines below, with an explanation in your own words of how they work.
 
+# The method count, looks at the string and counts the characters listed in the parenthesis ().
+# In this case, the count method is looking for iIagm.
+# The return is 11. Notice there is a capital I and lowercase i. They are technically different characters
+# and thus each need to be included in the () if they want to be counted.
+easy_enough.count("iIagm")
 
+# The prepend method takes what is in the () and adds it at the beginning of the string.
+# The return would be "Maybe I feel like I have a firm grasp on methods so far..."
+easy_enough.prepend ("Maybe ")
 
-# 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does, when at the end of a method name. Show your understading by providing an example and writing an explanation.
+# The empty? method checks to see if the string has any characters, or is indeed empty.
+# The return is false.
+easy_enough.empty?
+
+# The delete method will return a copy of the string, and will have deleted any
+# of the characters presented in between ""
+# The return here would be "Myb MybI fl lk I hv  frm grsp n mthds s fr..."
+easy_enough.delete "aeiou"
+
+# 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does,
+# when at the end of a method name. Show your understading by providing an example and writing an explanation.
+# Through a combination of trying my own code and research, I have found that including a '!' at the end
+# of a method is called a "Bang" method. Whatever method is run with `!` makes a permanent change in the variable.
+#Example below:
+easy_enough = "I feel like I have a firm grasp on methods so far..." # Creating the variable with a string
+puts easy_enough # Will show the string
+puts "" # Break
+puts easy_enough.upcase # Will show the string all in CAPS
+puts "" # Break
+puts easy_enough # Will show the string is back to its original format
+puts "" # Break
+puts easy_enough.upcase! # Here is the Bang Method `!`. The string will be in all CAPS
+puts ""
+puts easy_enough # This will show that the original string has been permanently changed to all CAPS
