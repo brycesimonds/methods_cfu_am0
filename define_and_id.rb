12d130ee490e3e85199b3e8b3c24b97dd8db1f7d
Bryce Simonds <bsimonds14@gmail.com>
2022-04-21 16:11:50 -0600

    Show ruby comments for methods

--- define_and_id.rb
+++ define_and_id.rb
@@ -8,16 +8,31 @@
 "Hello World".upcase
 
 
+#The include? method is called on the string object "Hellow World"
+#The string was checked to see if included "Hello"
+#The return value is true
 "Hello World".include?("Hello")
 
 
+#The end_with method is called on the string object "Hello World"
+#The string was checked to see if ended with "Hello"
+#The return value is false
 "Hello World".end_with?("Hello")
 
 
+#The end_with method is called on the string object "Hello World"
+#The string was checked to see if ended with the last 3 characters being "rld"
+#The return value is true
 "Hello World".end_with?("rld")
 
 
+#The length method is called on the string object "Hello World"
+#The string was checked to see how many characters are in the entire string
+#The return value is 11
 "Hello World".length
 
 
+#The size method is called on the string object "Hello World"
+#The string was checked to see how many characters are in the entire string
+#The return value is 11
 "Hello World".size
